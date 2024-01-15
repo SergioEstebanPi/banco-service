2bfe94812974398308f5561f77fa0ec8a0a5ffa8
Sergio Pina <sepi_147@hotmail.com>
2024-01-14 21:06:27 -0500

    Agrego dtos

---
+++ src/main/java/com/cqrs/banco/commonapi/dtos/CreateAccountRequestDTO.java
@@ -0,0 +1,15 @@
+package com.cqrs.banco.commonapi.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAccountRequestDTO {
+    private String currency;
+    private double amount;
+}
