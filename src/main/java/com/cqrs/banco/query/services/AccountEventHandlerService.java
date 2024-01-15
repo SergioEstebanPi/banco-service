ebe97b057bf14ee0a0f2e5f009910f1548dc8d44
Sergio Pina <sepi_147@hotmail.com>
2024-01-14 22:25:00 -0500

    Agrego query handlers

--- src/main/java/com/cqrs/banco/query/services/AccountEventHandlerService.java
+++ src/main/java/com/cqrs/banco/query/services/AccountEventHandlerService.java
@@ -8,15 +8,20 @@ import com.cqrs.banco.query.dto.AccountWatchEvent;
 import com.cqrs.banco.query.entities.Account;
 import com.cqrs.banco.query.entities.AccountTransaction;
 import com.cqrs.banco.query.queries.GetAccountBalanceStream;
+import com.cqrs.banco.query.queries.GetAccountById;
+import com.cqrs.banco.query.queries.GetAllAccounts;
 import com.cqrs.banco.query.repository.AccountRepository;
 import com.cqrs.banco.query.repository.TransactionRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.queryhandling.QueryHandler;
 import org.axonframework.queryhandling.QueryUpdateEmitter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -97,6 +102,34 @@ public class AccountEventHandlerService {
 
         queryUpdateEmitter.emit(GetAccountBalanceStream.class, (query) ->
                 (query.getAccountId().equals(account.getId())), accountWatchEvent);
+    }
+
+    @QueryHandler
+    public List<Account> on(GetAllAccounts query){
+        return accountRepository.findAll();
+    }
+
+    @QueryHandler
+    public Account on(GetAccountById query){
+        return accountRepository.findById(query.getAccountId()).get();
+    }
+
+    @QueryHandler
+    public AccountWatchEvent on(GetAccountBalanceStream query){
+        Account account = accountRepository.findById(query.getAccountId()).get();
+        AccountTransaction accountTransaction =
+                transactionRepository.findTop1ByAccountIdByTimestampDesc(query.getAccountId());
+
+        if(accountTransaction != null){
+            return new AccountWatchEvent(
+                    accountTransaction.getTimestamp(),
+                    account.getId(),
+                    account.getBalance(),
+                    accountTransaction.getTransactionType(),
+                    accountTransaction.getAmount()
+            );
+        }
+        return null;
     }
 
 }
