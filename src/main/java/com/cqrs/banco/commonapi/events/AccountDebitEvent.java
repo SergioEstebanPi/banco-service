ff71a91f2ee5931269071d8831d655d0aea93c88
Sergio Pina <sepi_147@hotmail.com>
2024-01-14 21:15:25 -0500

    Agrego eventos

---
+++ src/main/java/com/cqrs/banco/commonapi/events/AccountDebitEvent.java
@@ -0,0 +1,17 @@
+package com.cqrs.banco.commonapi.events;
+
+import lombok.Getter;
+
+public class AccountDebitEvent extends BaseEvent<String> {
+
+    @Getter
+    private String currency;
+    @Getter
+    private double amount;
+
+    public AccountDebitEvent(String id, String currency, double amount) {
+        super(id);
+        this.currency = currency;
+        this.amount = amount;
+    }
+}
